93e51c563a5e00433ec7779d6af7e022fe0d77b8
Frank23Woo <frank23woo@gmail.com>
2018-01-08 17:35:19 +0800

    Revert "Revert "refactor auth related""
    This reverts commit 176e7feb5851973dd98c4bdfb35445ccfcb7a8ef.

--- mdvn-common/src/main/java/com/mdvns/mdvn/common/bean/model/StaffAuthInfo.java
+++ mdvn-common/src/main/java/com/mdvns/mdvn/common/bean/model/StaffAuthInfo.java
@@ -13,13 +13,16 @@ public class StaffAuthInfo {
     private String projId;
 
     /*员工Id*/
-    private String staffId;
+//    private String staffId;
+    private Long staffId;
 
     /*权限编号*/
     private Integer authCode;
 
-    private  String assignerId;
-    
-    private String hierarchyId;
+//    private  String assignerId;
+    private  Long assignerId;
+
+//    private String hierarchyId;
+    private Long hostId;
 
 }
