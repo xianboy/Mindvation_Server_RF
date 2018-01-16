9cf59029275ac286980ee9782da69c436877c6b8
xianboy2 <xianboy2@sina.com>
2018-01-16 16:47:03 +0800

    update RetrieveService in mdvn-requirement

--- mdvn-requirement/src/main/java/com/mdvns/mdvn/requirement/service/RetrieveService.java
+++ mdvn-requirement/src/main/java/com/mdvns/mdvn/requirement/service/RetrieveService.java
@@ -8,11 +8,7 @@ import java.util.List;
 
 public interface RetrieveService {
 
-    /**
-     * 查询指定project下的requirement列表:支持分页
-     * @param singleArgRequest
-     * @return
-     */
+    //查询指定project下的requirement列表:支持分页
     RestResponse<?> retrieveListByHostSerialNo(SingleCriterionRequest singleArgRequest);
 
     /*根据id获取详情*/
@@ -25,8 +21,14 @@ public interface RetrieveService {
     Long retrieveLabelIdBySerialNo(SingleCriterionRequest retrieveRequest);
 
     List<Long> retrieveReqMembersBySerialNo(SingleCriterionRequest singleCriterionRequest) throws BusinessException;
-<<<<<<< HEAD
-=======
+
+    RestResponse<?> retrieveReqMembersInfoBySerialNo(SingleCriterionRequest singleCriterionRequest) throws BusinessException;
+
+    //获取指定编号的需求的过程方法id
+    Long retrieveLabelIdBySerialNo(SingleCriterionRequest retrieveRequest);
+
+
+    List<Long> retrieveReqMembersBySerialNo(SingleCriterionRequest singleCriterionRequest) throws BusinessException;
 
     //获取指定过程方法对应的需求编号
     List<String> retrieveSerialNoByLabel(RetrieveReqmtByLabelRequest retrieveRequest);
@@ -36,5 +38,4 @@ public interface RetrieveService {
 
     //获取指定需求集合的dashboard
     ReqmtDashboard retrieveDashboard(RetrieveMvpContentRequest retrieveRequest);
->>>>>>> parent of c74f720... Merge branch 'master' of https://github.com/xianboy/Mindvation_Server_RF
 }
