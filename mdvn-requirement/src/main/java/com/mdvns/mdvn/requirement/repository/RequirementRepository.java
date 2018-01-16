b47484b280cf8cc56c41870aafe3377b16c49888
xianboy2 <xianboy2@sina.com>
2018-01-16 16:43:36 +0800

    update requirement without conflict

--- mdvn-requirement/src/main/java/com/mdvns/mdvn/requirement/repository/RequirementRepository.java
+++ mdvn-requirement/src/main/java/com/mdvns/mdvn/requirement/repository/RequirementRepository.java
@@ -43,4 +43,30 @@ public interface RequirementRepository extends JpaRepository<Requirement, Long> 
     //根据编号获取functionLabelId
     @Query("select r.functionLabelId from Requirement r where r.serialNo=?1 and r.isDeleted=?2")
     Long findLabelIdBySerialNoAndIsDeleted(String serialNo, Integer isDeleted);
+
+
+    //获取指定id的过程方法对应的需求编号
+    @Query("select r.serialNo from Requirement r where r.hostSerialNo=?1 and r.functionLabelId in ?2 and r.isDeleted=?3")
+    List<String> findSerialNoByHostSerialNoAndFunctionLabelIdInAndIsDeleted(String hostSerialNo, List<Long> labels, Integer isDeleted);
+
+    //获取指定hostSerialNo(项目编号)和templateId的需求编号
+    @Query("select r.serialNo from Requirement r where r.hostSerialNo=?1 and r.templateId=?2 and r.isDeleted=?3")
+    List<String> findSerialNoByHostSerialNoAndTemplateIdAndIsDeleted(String hostSerialNo, Long templateId, Integer isDeleted);
+
+    //更新指定集合的需求的mvpId(三层)
+    @Modifying
+    @Query("update Requirement r set r.mvpId = ?1 where r.serialNo in ?2")
+    void updateMvpIdBySerialNoIn(Long mvpId, List<String> requirements);
+
+    //修改指定id集合的需求的mvpId
+    @Modifying
+    @Query("update Requirement r set r.mvpId =?1 where r.id in ?2")
+    void updateMvpIdByIdIn(Long mvpId, List<Long> contents);
+
+
+    //获取指定requirement集合中mvpId 为空的数据
+    List<Requirement> findBySerialNoInAndIsDeletedAndMvpIdIsNull(List<String> serialNoList, Integer isDeleted);
+
+    //查询指定requirement集合中指定mvpId的数据
+    List<Requirement> findBySerialNoInAndIsDeletedAndMvpId(List<String> serialNoList, Integer isDeleted, Long mvpId);
 }
